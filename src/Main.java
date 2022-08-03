2c9e46cad72e0fae498e018696d3c8194786e7c5
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-03 13:26:18 +0300

    supermiles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 4000;
+        int y = 20;
+        System.out.println(x + " руб" + " : Cтоимость билета");
+        System.out.println(y + " руб" + " = 1 миля");
+
+        int z = x / y;
+        System.out.println(z + " : Новое количество миль");
+    }
+}
