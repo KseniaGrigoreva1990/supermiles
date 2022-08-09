52b8659b36ffe017d46bb0358b4ba13d4b6b3ca2
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-09 18:29:47 +0300

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 4000;
-        int y = 20;
-        System.out.println(x + " руб" + " : Cтоимость билета");
-        System.out.println(y + " руб" + " = 1 миля");
+        int ticketCost = 6000;
+        int bonusMile = 20;
+        System.out.println(ticketCost + " руб" + " : Cтоимость билета");
+        System.out.println(bonusMile + " руб" + " = 1 миля");
 
-        int z = x / y;
-        System.out.println(z + " : Новое количество миль");
+        int result = ticketCost / bonusMile;
+        System.out.println(result + " : Новое количество миль");
     }
 }
+
