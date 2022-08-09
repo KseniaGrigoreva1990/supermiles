157bf2ebf97378571207ca459c5bdd80cec1e1ad
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-09 17:48:25 +0300

    supermiles (w/changes)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 4000;
+        int bonusMile = 20;
+        System.out.println(ticketCost + " руб" + " : Cтоимость билета");
+        System.out.println(bonusMile + " руб" + " = 1 миля");
+
+        int result = ticketCost / bonusMile;
+        System.out.println(result + " : Новое количество миль");
+    }
+}
